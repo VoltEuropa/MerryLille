6f102a2172898b07b7bd24a32e10e590cdca4c03
frequent <svenfranck@yahoo.com>
2019-12-01 23:07:56 +0000

    door 1 test

--- gadget_volt.js
+++ gadget_volt.js
@@ -17,18 +17,29 @@
   var DIALOG_ACTIVE = "volt-dialog-active";
   var LOCATION = window.location;
   var DOCUMENT = window.document;
+  var HIDDEN = "volt-hidden";
   var INTERSECTION_OBSERVER = window.IntersectionObserver;
   var TEMPLATE_PARSER = /\{([^{}]*)\}/g;
-  var POPPER = "width=600,height=480,resizable=yes,scrollbars=yes,status=yes";
   var LANG = "https://raw.githubusercontent.com/VoltEuropa/MerryVolt/master/lang/";
-  var SOCIAL_MEDIA_CONFIG = {
-    "facebook": "https://www.facebook.com/sharer.php?u={url}",
-    "twitter": "https://twitter.com/intent/tweet?url={url}&text={text}&hashtags={tag_list}"
-  };
 
   /////////////////////////////
   // methods
   /////////////////////////////
+  function setCookie (name, value, days, session_only) {
+    var d;
+    if (session_only) {
+      DOCUMENT.cookie = name + "=" + value + ";path=/;";
+    }
+    d = new Date();
+    d.setTime(d.getTime() + 24*60*60*1000*days);
+    DOCUMENT.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
+  }
+
+  function getCookie(name) {
+    var v = DOCUMENT.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return v ? v[2] : null;
+  }
+
   function getElem(my_element, my_selector) {
     return my_element.querySelector(my_selector);
   }
@@ -83,37 +94,13 @@
   function getLang(nav) {
     return (nav.languages ? nav.languages[0] : (nav.language || nav.userLanguage));
   }
-  /*
-  function waitForServiceWorkerActive(registration) {
-    var serviceWorker;
-    if (registration.installing) {
-      serviceWorker = registration.installing;
-    } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-    } else if (registration.active) {
-      serviceWorker = registration.active;
-    }
-    if (serviceWorker.state !== "activated") {
-      return RSVP.Promise(function (resolve, reject) {
-        serviceWorker.addEventListener('statechange', function (e) {
-          if (e.target.state === "activated") {
-            resolve();
-          }
-        });
-        RSVP.delay(500).then(function () {
-          reject(new Error("Timeout service worker install"));
-        });
-      });
-    }
-  }
-  */
 
   function getConfig(my_language) {
     return {
       "type": "volt_storage",
       "repo": "MerryVolt",
-      "path": "lang/" + my_language,
-      "__debug": "https://softinst103163.host.vifib.net/public/MerryLille/lang/" + my_language + "/debug.json"
+      "path": "lang/" + my_language
+      //"__debug": "https://softinst103163.host.vifib.net/public/MerryLille/lang/" + my_language + "/debug.json"
     };
   }
 
@@ -137,6 +124,7 @@
         "unset": getElem(gadget.element, ".xmas-door_clear"),
         "dialog": getElem(gadget.element, ".volt-dialog"),
         "dialog_content": getElem(gadget.element, ".volt-dialog-content"),
+        "consent_wrapper": getElem(gadget.element,".volt-cookie__notice-wrapper"),
 
         // yaya, should be localstorage caling repair to sync
         "url_dict": {},
@@ -175,34 +163,6 @@
       return this.route(VOLT, "allDocs");
     })
 
-    /*
-    .declareMethod("installServiceWorker", function () {
-      var gadget = this;
-      if (navigator.serviceWorker === undefined) {
-        return;
-      }
-      return new RSVP.Queue()
-        .push(function () {
-          return navigator.serviceWorker.register("serviceworker.js");
-        })
-        .push(function (registration) {
-          return waitForServiceWorkerActive(registration);
-        })
-        .push(null, function (error) {
-          return new RSVP.Queue()
-            .push(function () {
-              return gadget.stateChange("sw_errors", gadget.state.sw_errors + 1);
-            })
-            .push(function () {
-              return RSVP.delay(1000);
-            })
-            .push(function () {
-              return gadget.installServiceWorker();
-            });
-        });
-    })
-    */
-
     .declareMethod("stateChange", function (delta) {
       var gadget = this;
       var state = gadget.state;
@@ -221,111 +181,6 @@
       //if (delta.hasOwnProperty("sw_errors")) {
       //  state.sw_errors = delta.sw_errors;
       //}
-      return;
-    })
-
-    // thx: https://css-tricks.com/simple-social-sharing-links/
-    // twitter prevalidate url: https://cards-dev.twitter.com/validator
-    // https://developers.facebook.com/docs/sharing/best-practices/
-    .declareMethod("shareUrl", function (my_scm) {
-      var popup;
-      var is_mobile = window.matchMedia("only screen and (max-width: 48em)");
-      var popup_resolver;
-
-      // lots of bells and whistles for trying to stay on the page, use this
-      // with localstorage is we want to keep state or login on social media
-      var resolver = new Promise(function (resolve, reject) {
-        popup_resolver = function resolver(href) {
-          return resolve({});
-        };
-      });
-
-      popup = window.open(
-        SOCIAL_MEDIA_CONFIG[my_scm].supplant({
-          "url": window.encodeURIComponent(LOCATION.href),
-          "text":"",
-          "tag_list": "VoteVolt,Europe"
-        }),
-        is_mobile.matches ? BLANK : STR,
-        is_mobile.matches ? null : POPPER
-      );
-      popup.opener.popup_resolver = popup_resolver;
-      return window.promiseEventListener(popup, "load", true);
-    })
-
-    .declareMethod("showDialog", function (my_event) {
-      var gadget = this;
-      var dict = gadget.property_dict;
-      var dialog = dict.dialog;
-      var target = dict.url_dict[my_event.target.value];
-
-      if (my_event.target.parentElement.classList.contains("is-locked")) {
-        return;
-      }
-      if (!target || target.indexOf("http") === -1 || gadget.state.online === false) {
-        return;
-      }
-
-      return new RSVP.Queue()
-        .push(function () {
-          return gadget.volt_get(target);
-        })
-        .push(function (my_data) {
-          var tab_head = STR;
-          var tab_content = STR;
-          var topic;
-          var i;
-
-          for (i = 0; i < my_data.topic_list.length; i += 1) {
-            topic = my_data.topic_list[i];
-            topic.id = i + 1;
-            topic.active = i === 0 ? ACTIVE : STR;
-            topic.title = my_data.title;
-            topic.slug = "tab-" + Math.round(Math.random()*1000000,0);
-            //window.encodeURIComponent(
-            //  topic.proposal.substring(0,10).split(" ").join("-").toLowerCase()
-            //);
-            tab_head += getTemplate(KLASS, "dialog_tab_header").supplant(topic);
-
-            // new year
-            if (target.indexOf("31.json") > -1) {
-              tab_content += getTemplate(KLASS, "dialog_ny_content").supplant(topic);
-            } else {
-              tab_content += getTemplate(KLASS, "dialog_tab_content").supplant(topic);
-            }
-          }
-
-          setDom(dict.dialog_content, getTemplate(KLASS, "dialog_template").supplant({
-            "tab_list": tab_head,
-            "content_list": tab_content
-          }), true);
-
-          window.componentHandler.upgradeElements(dialog);
-          return gadget.translateDom(dict.i18n_dict, dialog);
-        })
-        .push(function () {
-          return gadget.handleDialog();
-        });
-    })
-
-    .declareMethod("handleDialog", function (keyBoardOverride) {
-      var gadget = this;
-      var dict = gadget.property_dict;
-      var dialog = getElem(gadget.element, (".volt-dialog"));
-      var active_element = DOCUMENT.activeElement;
-
-      function closeDialog() {
-        dialog.classList.remove(DIALOG_ACTIVE);
-        dict.unset.checked = true;
-        return;
-      }
-
-      if (dialog.classList.contains(DIALOG_ACTIVE)) {
-        return closeDialog();
-      }
-      if (!dialog.classList.contains(DIALOG_ACTIVE)) {
-        dialog.classList.add(DIALOG_ACTIVE);
-      }
       return;
     })
 
@@ -363,6 +218,12 @@
         });
     })
 
+    .declareMethod("storeCoookieConsent", function (my_event) {
+      var gadget = this;
+      setCookie("consent", 1, 60);
+      gadget.property_dict.consent_wrapper.classList.add(HIDDEN);
+    })
+
     .declareMethod("buildCalendarLookupDict", function () {
       var gadget = this;
       var dict = gadget.property_dict;
@@ -389,29 +250,6 @@
           }
           dict.url_dict["ui"] = LANG + gadget.state.locale + "/ui.json";
         });
-    })
-
-    .declareMethod("switchTab", function (dir) {
-      var gadget = this;
-      var elem = gadget.element;
-      var tabs = elem.querySelectorAll(".mdl-tabs__tab");
-      var panels = elem.querySelectorAll(".mdl-tabs__panel");
-      var i;
-      var len;
-      var index;
-      for (i = 0, len = tabs.length; i < len; i += 1) {
-        if (tabs[i].classList.contains(ACTIVE)) {
-          if (dir === 1) {
-            index = tabs[i + 1] ? i + 1 : 0;  
-          } else {
-            index = tabs[i - 1] ? i - 1 : len - 1;
-          }
-          tabs[i].classList.remove(ACTIVE);
-          panels[i].classList.remove(ACTIVE);
-        }
-      }
-      tabs[index].classList.add(ACTIVE);
-      panels[index].classList.add(ACTIVE);
     })
 
     // -------------------.--- Render ------------------------------------------
@@ -445,7 +283,9 @@
     .declareService(function () {
       var body = DOCUMENT.body;
       var seo = body.querySelector(".volt-seo-content");
-      seo.parentElement.removeChild(seo);
+      if (seo) {
+        seo.parentElement.removeChild(seo);
+      }
       body.classList.remove("volt-splash");     
     })
 
@@ -455,6 +295,19 @@
     .declareService(function () {
       var gadget = this;
       var listener = window.loopEventListener;
+      var dict = gadget.property_dict;
+      /*
+      if (getCookie("init") === null) {
+        setCookie("init", 1, 1, true);
+      }
+      */
+      if (getCookie("consent") === null) {
+
+        // quick fix, ensure cookie expiring doesn't break page
+        if (dict.consent_wrapper) {
+          dict.consent_wrapper.classList.remove(HIDDEN);
+        }
+      }
 
       function handleConnection() {
         return gadget.stateChange({"online": window.navigator.onLine});
@@ -469,60 +322,15 @@
     /////////////////////////////
     // on Event
     /////////////////////////////
-
-    .onEvent("change", function (event) {
-      return this.showDialog(event);
-    })
-
     .onEvent("submit", function (event) {
       switch (event.target.getAttribute(NAME)) {
-        case "volt-dialog-close":
-          return this.handleDialog();
-        case "volt-dialog-prev":
-          return this.switchTab(-1);
-        case "volt-dialog-next":
-          return this.switchTab(1);
-        case "volt-share-facebook":
-          return this.shareUrl("facebook");
-        case "volt-share-twitter":
-          return this.shareUrl("twitter");
-        case "volt-share-linkedin":
-          return this.shareUrl("linkedin");
         case "volt-select-language":
           return this.updateStorage(event.target.volt_language.value);
+        case "volt-cookie__consent":
+          return this.storeCoookieConsent(event.target);
+      
       }
-    })
-
-    /*
-    .onEvent("beforeinstallprompt", function (event) {
-      var gadget = this;
-      var deferredPrompt;
-      var btn = getElem(gadget.element, ".volt-homescreen-flag");
-
-      function handleClick(my_click_event) {
-        btn.style.display = "none";
-        deferredPrompt.prompt();
-        deferredPrompt.userChose.then(function (choise_result) {
-          deferredPrompt = null;
-          if (choise_result.outcome === 'accepted') {
-            return;  
-          }
-          return;
-        });
-      }
-
-      event.preventDefault();
-      btn.style.display = "block";
-      deferredPrompt = e;
-      return window.loopEventListener(btn, "click", false, handleClick);
-    }, false, false)
-    */
-
-    .onEvent("keydown", function (event) {
-      if (event.key === "Escape" || event.key === "Esc" || event.keyCode === 27) {
-        return this.handleDialog(true);
-      }
-    }, false, false);
+    });
 
 
 }(window, rJS, RSVP, Math));
